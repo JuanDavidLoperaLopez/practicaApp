a2323fb879ed9db43ba3fa049e07099457f7da62
juan david lopera lopez <juandavidl2011@hotmail.es>
2016-05-08 00:35:00 -0500

    second commit, in this commit, i did some changes , go from first view to second view using login -estoy practicando mi ingles  :)

---
+++ TrainWithGit/TrainWithGit/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  TrainWithGit
+//
+//  Created by juan david lopera lopez on 5/8/16.
+//  Copyright © 2016 juan david lopera lopez. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TrainWithGit/TrainWithGit/ViewController.swift
+++ TrainWithGit/TrainWithGit/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    //MARK: - IBActions
 
+    @IBAction func EntrarBtn(sender: AnyObject) {
+        if nameTextField.text == "juan" && passwordTextField.text == "1234"{
+            let datos:[String] = ["juan","1234"]
+            performSegueWithIdentifier("uno", sender: datos)
+        }else{
+            
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
