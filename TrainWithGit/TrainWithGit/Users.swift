f98954c8fdef0e20b9116963d3641a76baa328fd
juan david lopera lopez <juandavidl2011@hotmail.es>
2016-05-09 01:36:29 -0500

    commit 3, i try do logging

---
+++ TrainWithGit/TrainWithGit/Users.swift
@@ -0,0 +1,21 @@
+//
+//  Users.swift
+//  TrainWithGit
+//
+//  Created by juan david lopera lopez on 5/9/16.
+//  Copyright © 2016 juan david lopera lopez. All rights reserved.
+//
+
+import Foundation
+
+class Users {
+    var name:String
+    var password:String
+    
+    init(name:String, password:String){
+        self.name = name
+        self.password = password
+    }
+    
+    
+}
